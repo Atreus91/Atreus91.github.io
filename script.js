86e2689a8117ed05afad4779bfb1bd32ada1e4ef
clement poissonnet <clement.poissonnet@gmail.com>
2025-02-23 19:05:36 +0100

    style and script

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Documentation Data Science chargée avec succès !");
+    // Vous pourrez ajouter ici des fonctionnalités interactives,
+    // comme des animations, des filtres ou des recherches dynamiques.
+  });
+  
